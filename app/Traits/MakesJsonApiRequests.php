2b20e28acbe6d829a53bd9c16368c89787750660
Roter-S <bryan.sosof@gmail.com>
2024-04-25 01:14:22 -0600

    ci: folder creation "Traits"

--- app/Traits/MakesJsonApiRequests.php
+++ app/Traits/MakesJsonApiRequests.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests;
+namespace App\Traits;
 
 use Illuminate\Testing\TestResponse;
 
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Traits\MakesJsonApiRequests;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
